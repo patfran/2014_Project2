84d0243fca925e90c66894e3d554c3705e32572a
Patrick P Francisco unknown student <ppf3@afsconnect1.njit.edu>
2014-12-15 11:18:42 -0500

    fixed contact form

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,34 @@
-  (function($,W,D)
-  {
+(function($,W,D)
+{
     var JQUERY4U = {};
 
     JQUERY4U.UTIL =
     {
-
-      setupFormValidation: function()
-      {
-
-        $("#register-form").validate({
-	    rules: {
-	        firstname: "required",
-		lastname: "required",
-		email: {
-		    required: true,
-		    email: true
-		},
-		password: {
-		    required: true,
-		    minlength: 8
-		},
-		agree: "required"
-	},
-	messages: {
-	    firstname: "Enter your first name",
-	    lastname: "Enter your last name",
-	    password: {
-	    	required: "Provide a password",
-		minlength: "Password must be at least 8 characters"
-	    },
-	    email: "Enter a valid email address",
-	    agree: "Accept our policy"
-	},
-	submitHandler: function(form){
-	    form.submit();
+		setupFormValidation: function()
+		{
+			$("#register-form").validate({
+				rules: {
+					subject: "required",
+					message: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					agree: "required"
+				},
+				messages: {
+					subject: "Please enter the subject",
+					message: "Please enter a message",
+					email: "Please enter a valid email address",
+				},
+				submitHandler: function(form){
+					form.submit();
+				}
+			});
+		}
 	}
-     });
-    }
-  }
 
-  $(D).ready(function($){
-      JQUERY4U.UTIL.setupFormValidation();
-  });
+	$(D).ready(function($){
+		JQUERY4U.UTIL.setupFormValidation();
+	});
 })(jQuery, window, document);
